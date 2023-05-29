81c46e84ba31c68f8110a337a5953fe7c5871328
Robiii <robin.chabert@epitech.eu>
2023-05-29 23:45:21 +0200

    [ADD] components for the home page

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,15 @@
-import InDev from "@/components/modules/InDev";
+import dynamic from 'next/dynamic';
+
+const InDev = dynamic(() => import('@/components/modules/InDev'));
+const HomePage = dynamic(() => import('@/components/modules/HomePage'));
+const Skills = dynamic(() => import('@/components/modules/Skills'))
+const Jobs = dynamic(() => import('@/components/modules/Jobs'))
+const Contact = dynamic(() => import('@/components/modules/Contact'))
 
 export const components = {
-    inDev: InDev,
+  inDev: InDev,
+  HomePage: HomePage,
+  Skills: Skills,
+  Jobs: Jobs,
+  Contact: Contact,
 };
